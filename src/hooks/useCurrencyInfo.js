36a6d40533753e2af7149201a48d25ccbbcf575d
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2024-04-24 00:34:54 +0500

    create a corruncy convertor app using react

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((jsonRes) => setData(jsonRes[currency]));
+    }, [currency]);
     
+    return data;
 }
+
+export default useCurrencyInfo;
